8732c965a4a3cbc2b06bec1f84915c9739f7b57a
Alyssa Diaz <alydiaz77@gmail.com>
2016-08-03 21:27:28 -0500

    topojson conversion and basic map beginnings

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is an app that will display Chicago Crime data from the [Chicago Tribune 'Crime in Chicago' API](https://github.com/newsapps/chicagocrime)
 
 This will utilize the [d3js library](https://d3js.org/) to display a chloropleth of the 77 Chicago community Areas.
+
+Using [this tutorial](https://bost.ocks.org/mike/map/) to start the first steps.
--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-// Load D3 chart here
+var width = 960,
+    height = 1160;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var projection = d3.geo.albers()
+  .center([0, 41.83])
+  .rotate([87.65, 0])
+  .parallels([40, 45])
+  .scale(90000)
+  .translate([width / 2, height / 2])
+
+var path = d3.geo.path()
+    .projection(projection);
+
+d3.json("js/chi_comm_areas_topo.json", function(error, chi) {
+  if (error) return console.error(error);
+  svg.append("path")
+     .datum(topojson.feature(chi, chi.objects.chi_community_areas))
+     .attr("d", path);
+});
+
