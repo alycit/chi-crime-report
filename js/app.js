06d7cf98027160b3a2417f9e2757cb5e33545090
Alyssa Diaz <alydiaz77@gmail.com>
2016-08-05 19:28:50 -0500

    Adds community area boundaries

--- js/app.js
+++ js/app.js
@@ -17,8 +17,17 @@ var path = d3.geo.path()
 
 d3.json("js/chi_comm_areas_topo.json", function(error, chi) {
   if (error) return console.error(error);
-  svg.append("path")
-     .datum(topojson.feature(chi, chi.objects.chi_community_areas))
+
+  svg.selectAll(".community_area")
+    .data(topojson.feature(chi, chi.objects.chi_community_areas).features)
+    .enter().append("path")
+     .attr("class", function(d){ return "community_area " + d.id})
      .attr("d", path);
+
+  svg.append("path")
+    .datum(topojson.mesh(chi, chi.objects.chi_community_areas, function(a, b) { return a !== b }))
+    .attr("d", path)
+    .attr("class", "community_area_boundary");
 });
+
 
